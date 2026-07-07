1e88cf3d0247b9ff77facc4ad577b77f17bbd1b5
Reza-ashrafi <115076971+Reza-ashrafi@users.noreply.github.com>
2026-07-07 21:13:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from config.settings import PROJECT_NAME
+from collectors.symbol_collector import SymbolCollector
 
 
 def main():
+
     print(f"{PROJECT_NAME} Started")
+
+    collector = SymbolCollector()
+
+    symbols = collector.collect()
+
+    print(symbols[:500])
 
 
 if __name__ == "__main__":
